41948e538665f72fe419c9c97f9479cd55a1310c
Gian <77172995+giansp@users.noreply.github.com>
2025-06-16 17:16:31 -0300

    Add files via upload

---
+++ ejercicio_dos/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace masmenoscero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            Console.WriteLine("Ingrese un número:");
+            num=Convert.ToDouble(Console.ReadLine());
+            if (num < 0)
+            {
+                Console.WriteLine("El número es negativo.");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("El número es positivo.");
+            }
+            else
+            {
+                Console.WriteLine("El número es cero.");
+            }
+        }
+    }
+}
