ef4a8f9dbb5f793453066fc5a84213f0cb25524a
Gian <77172995+giansp@users.noreply.github.com>
2025-06-16 17:54:54 -0300

    Add files via upload

---
+++ ejercicio_siete/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace bisiesto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio;
+            Console.WriteLine("Ingrese el año");
+            anio=Convert.ToInt32(Console.ReadLine());
+            if (anio % 4 == 0)
+            {
+                if (anio % 100 == 0)
+                {
+                    if (anio % 400 == 0)
+                    {
+                        Console.WriteLine("El año es bisiesto");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El año no es bisiesto");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El año es bisiesto");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El año no es bisiesto");
+            }
+        }
+    }
+}
