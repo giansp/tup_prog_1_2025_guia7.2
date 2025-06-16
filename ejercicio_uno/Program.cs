795c4d1916898c7b12a44ad8cd610c44e8a2f07a
Gian <77172995+giansp@users.noreply.github.com>
2025-06-16 17:12:42 -0300

    Add files via upload

---
+++ ejercicio_uno/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Condicion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double condicion,p1,p2,p3,promedio;
+            Console.WriteLine("Ingrese condición de promoción");
+            condicion = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese las tres notas a a evaluar");
+            p1 = Convert.ToDouble(Console.ReadLine());
+            p2 = Convert.ToDouble(Console.ReadLine());
+            p3 = Convert.ToDouble(Console.ReadLine());
+            promedio = (p1 + p2 + p3) / 3;
+            if (promedio >=condicion)
+            {
+                Console.WriteLine("Promociona");
+            }
+            else
+            {
+                Console.WriteLine("No promociona");
+            }
+        }
+    }
+}
