7553d68a19906ab7b8dfb6c5c52c994d74c027a2
Gian <77172995+giansp@users.noreply.github.com>
2025-06-16 17:37:18 -0300

    Add files via upload

---
+++ ejercicio_cuatro/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace mayorvalor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double mayor, val;
+            Console.WriteLine("Ingrese el valor 1:");
+            mayor = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor 2:");
+            val = Convert.ToDouble(Console.ReadLine());
+            if (val>mayor)
+            {
+                mayor = val;
+            }
+            Console.WriteLine("Ingrese el valor 3:");
+            val = Convert.ToDouble(Console.ReadLine());
+            if (val > mayor)
+            {
+                mayor = val;
+            }
+            Console.WriteLine("Ingrese el valor 4:");
+            val = Convert.ToDouble(Console.ReadLine());
+            if (val > mayor)
+            {
+                mayor = val;
+            }
+            Console.WriteLine("Ingrese el valor 5:");
+            val = Convert.ToDouble(Console.ReadLine());
+            if (val > mayor)
+            {
+                mayor = val;
+            }
+            Console.WriteLine("El mayor valor es: " + mayor);
+        }
+    }
+}
